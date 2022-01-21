c243442ccc4ddbc83258e924f9eabfe90a02940d
mshahiq <m.shahiqmukhtar@gmail.com>
2022-01-22 03:59:02 +0500

    Update challenge.py

--- Challenge-22.01/challenge.py
+++ Challenge-22.01/challenge.py
@@ -1,19 +1,18 @@
-def print_me(array):
+def try_me(lst):
 
-    for x in array: 
-        
-        if x%3 == 0 and x%5 == 0:
-            array.remove(x)
-            array.insert((x-3),"Strive School")
+    for i in lst:
 
-        elif x%3 == 0:
-            array.remove(x)
-            array.insert((x-1),"Strive")
+        if (i%3==0) and (i%5==0):
+            d_35 = print("Strive School")
+        elif (i%3==0):
+            d_3 = print("Strive")
+        elif (i%5==0):
+            d_5 = print("School")
+        else:
+            d_on = print(i)                     #d_on: number not divisible by 3 neither by 5
 
-        elif x%5 == 0:
-            array.remove(x)
-            array.insert((x-2),"School")       
-    return array
+    return d_35,d_3,d_5,d_on
 
-    
-print(print_me(([1,2,3,5,15])))
+lst = [3,3,3,9,15,25,1,2,2,30,54]
+
+try_me(lst)
