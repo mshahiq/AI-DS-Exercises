39d40dc689364aac2aa734df90eecf0910d7e315
mshahiq <m.shahiqmukhtar@gmail.com>
2022-01-22 00:48:20 +0500

    Update challenge.py

--- Challenge-22.01/challenge.py
+++ Challenge-22.01/challenge.py
@@ -1,14 +1,13 @@
 def print_me(array):
 
     for x in array: 
-        
         if x == 3:
             array.remove(x)
             array.insert((x-1),"Strive")
 
         elif x == 5:
             array.remove(x)
-            array.insert((x-2),"School")
+            array.insert((x-2),"School"s)
 
         elif x == 30:
             array.remove(x)
@@ -17,4 +16,4 @@ def print_me(array):
     return array
 
     
-print(print_me(([1,2,3,5,10,15,30])))
+print(print_me(([1,2,3,5,10,30])))
