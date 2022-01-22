b348ff0d9547e3beea3d1a63537ae8a353e557b6
mshahiq <m.shahiqmukhtar@gmail.com>
2022-01-22 23:00:49 +0500

    Update challenge_230122.py

--- Challenge-22.01/challenge_230122.py
+++ Challenge-22.01/challenge_230122.py
@@ -1,10 +1,15 @@
 
 def remove_duplicates(list_nu):
+
     for single_digit in list_nu:
+
         if list_nu.count(single_digit) == 1:
+
             print(single_digit)
 
+
 # list_nu = [1,1,2,2,3,3,4,4,5]
+
 list_nu = [1,2,3,4,1,3,4]
 
 remove_duplicates(list_nu)
